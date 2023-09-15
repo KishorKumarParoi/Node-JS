0aba6aab6675678a58fcda6668668cce2000650c
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-15 17:20:34 +0600

    package-lock.json

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const x = require("lodash");
+import x from "lodash";
 console.log("index.js");
 console.log("edit index.js");
+
+console.dir(x);
+console.dir(global);
+
+const a = 100;
+for (let x of [1, 2, 3]) {
+  console.log(x);
+}
+console.log(a);
