bf564b46ed4a3e32f26c35b4bad2470ea9cc58b4
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-15 18:54:59 +0600

    eslint prettier clash solved

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-import x from "lodash";
-console.log("index.js");
-console.log("edit index.js");
+import x from 'lodash';
+
+console.log('index.js');
+console.log('edit index.js');
 
 console.dir(x);
 console.dir(global);
 
 const a = 100;
-for (let x of [1, 2, 3]) {
-  console.log(x);
+const arr = [1, 2, 3];
+// eslint-disable-next-line no-restricted-syntax
+for (const val of arr) {
+    console.log(val);
 }
 console.log(a);
 
 console.log(x.findLast([1, 2, 3]));
+
+const kishor = 'I have solved issues of forced format document';
+console.log(kishor);
