a75eed6034c4f3cfadf4c0929bdcf7b098168d26
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-16 13:56:19 +0600

    added infinity imaged scrolling

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log('script.js');
+// eslint-disable-next-line no-undef
+const images = document.querySelectorAll('img');
+console.log(images);
+
+// eslint-disable-next-line no-undef
+const observer = new IntersectionObserver(
+    // eslint-disable-next-line no-shadow
+    (images) => {
+        images.forEach((img) => {
+            if (img.isIntersecting) {
+                img.target.classList.add('visible');
+            } else {
+                img.target.classList.remove('visible');
+            }
+        });
+    },
+    {
+        threshold: 0.8,
+    }
+);
+
+images.forEach((img) => observer.observe(img));
