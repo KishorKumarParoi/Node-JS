4c9307e57f7f97382086be3da8ccc6a445991230
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-18 05:58:46 +0600

    export/import

--- code/export.js
+++ code/export.js
@@ -1,11 +1,15 @@
 // const a = 100;
-// const arr = [1, 2, 3];
+const arr = [1, 2, 3];
 const people = ['Kishor', 'Arnab', 'Soumya'];
 
-// function test() {
-//     console.log('Hello World');
-// }
+function test() {
+    console.log('Hello World');
+}
 
 // console.log(global);
 
-export default people;
+export default {
+    people,
+    arr,
+    test,
+};
--- code/import.js
+++ code/import.js
@@ -1,3 +1,4 @@
-import x from './export';
+/* eslint-disable import/extensions */
+import x from './export.js';
 
 console.log(x);
