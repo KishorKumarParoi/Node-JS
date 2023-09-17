3e072e69b77d2b69e6f36d56a2e465fc19317457
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-18 05:34:45 +0600

    updated

---
+++ code/export.js
@@ -0,0 +1,11 @@
+// const a = 100;
+// const arr = [1, 2, 3];
+const people = ['Kishor', 'Arnab', 'Soumya'];
+
+// function test() {
+//     console.log('Hello World');
+// }
+
+// console.log(global);
+
+export default people;
