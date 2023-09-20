2deba1457a97b3dc9ca0f1b0f6438545c4581623
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-20 21:13:10 +0600

    fs write file

--- code/fileSystemModule.js
+++ code/fileSystemModule.js
@@ -16,18 +16,48 @@ console.log(module);
 // }).listen(8080);
 
 http.createServer((req, res) => {
-    fs.appendFile('demo1.txt', 'Hello World!', (err) => {
-        if (err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('Saved Hello World!');
-        res.end();
-    });
+    // fs.appendFile('demo1.txt', 'Hello World!', (err) => {
+    //     if (err) throw err;
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.write('Saved Hello World!');
+    //     res.end();
+    // });
 
-    fs.appendFile('./demo.html', '<h1>Hello World!</h1>', (err, data) => {
+    // fs.appendFile('./demo.html', '<h1>Hello World!</h1>', (err, data) => {
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.write(data);
+    //     res.end();
+    // });
+
+    // fs.open('./demo.html', 'w', (err, file) => {
+    //     if (err) throw err;
+    //     console.log('Saved!');
+    //     console.log(file);
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.write('opened!');
+    //     res.end();
+    // });
+
+    fs.writeFile('./demo.html', '<h1>Hello World!</h1>', (err, data) => {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         res.end();
     });
 
-    
+    fs.readFile('./demo.html', (err, data) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(`Read Loudly!${data}`);
+        res.end();
+    });
+
+    // fs.unlink('./demo1.txt', (err) => {
+    //     if (err) throw err;
+    //     console.log('File deleted!');
+    // });
+
+    // fs.rename('./demo.html', './demo2.html', (err) => {
+    //     if (err) throw err;
+    //     console.log('File Renamed!');
+    // });
 }).listen(8080);
