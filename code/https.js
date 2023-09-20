6f5d64dde70f4d645dcf97c52956f64dec0c9325
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-20 15:05:30 +0600

    url method query string

--- code/https.js
+++ code/https.js
@@ -1,11 +1,15 @@
 /* eslint-disable import/extensions */
 import http from 'http';
+import url from 'url';
 import { arr } from './myFirstModule.js';
 
 console.log(arr);
 
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/html' });
+    const q = url.parse(req.url, true).query;
     res.write(`Arr : ${arr} <br>
-    URL : ${req.url}`);
+    URL : ${req.url} <br>
+    q.year : ${q.year} <br>
+    `);
 }).listen(8080);
