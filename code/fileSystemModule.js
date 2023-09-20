18ecad392cb5c42a1f2986fb9aef5b6c19c0ed8a
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-20 15:29:00 +0600

    fs append file

--- code/fileSystemModule.js
+++ code/fileSystemModule.js
@@ -7,10 +7,26 @@ console.log(fs);
 console.log(http.STATUS_CODES);
 console.log(module);
 
+// http.createServer((req, res) => {
+//     fs.readFile('./demo.html', (err, data) => {
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write(data);
+//         res.end();
+//     });
+// }).listen(8080);
+
 http.createServer((req, res) => {
-    fs.readFile('./demo.html', (err, data) => {
+    fs.appendFile('demo1.txt', 'Hello content!', (err) => {
+        if (err) throw err;
+        console.log('Saved Hello World!');
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write(data);
+        res.write('Saved!');
         res.end();
     });
+
+    // fs.readFile('./demo.html', (err, data) => {
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.write(data);
+    //     res.end();
+    // });
 }).listen(8080);
