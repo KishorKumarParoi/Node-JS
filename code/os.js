2f885c7d5c953aa823eaa709cd0adcb06c2d01e2
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-20 21:22:33 +0600

    os info

---
+++ code/os.js
@@ -0,0 +1,20 @@
+import * as os from 'os';
+
+console.log(os);
+console.log(os.EOL);
+console.log(os.freemem());
+console.log(os.homedir());
+console.log(os.hostname());
+console.log(os.networkInterfaces());
+console.log(os.platform());
+console.log(os.release());
+console.log(os.tmpdir());
+console.log(os.totalmem());
+console.log(os.type());
+console.log(os.userInfo());
+console.log(os.version());
+console.log(os.uptime());
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.endianness());
+console.log(os.loadavg());
