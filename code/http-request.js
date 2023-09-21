f69cf304b43f98ae8668f025102fb70b11ac5372
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-21 13:03:23 +0600

    opens and returns content

--- code/http-request.js
+++ code/http-request.js
@@ -5,18 +5,14 @@ import fs from 'fs';
 import http from 'http';
 import url from 'url';
 
-const address = 'http://localhost:8080/';
-
 http.createServer((req, res) => {
-    const q = url.parse(address.url, true);
-    const fileName = `.${q.pathname}`;
-
-    fs.readFile(fileName, (err, data) => {
+    const q = url.parse(req.url, true);
+    const filename = `.${q.pathname}`;
+    fs.readFile(filename, (err, data) => {
         if (err) {
             res.writeHead(404, { 'Content-Type': 'text/html' });
             return res.end('404 Not Found');
         }
-
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write(data);
         return res.end();
