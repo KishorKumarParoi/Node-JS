5c8dc8d3d2d43735b8adf2431e3bd250f4a35052
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-22 20:16:54 +0600

    get data of demo.html file

---
+++ fs.js
@@ -0,0 +1,11 @@
+import * as fs from 'fs';
+
+console.log(fs);
+
+fs.readFile('demo.html', (err, data) => {
+    if (err) throw err;
+    console.log(data.toString());
+    console.log('File read');
+    console.log(JSON.stringify(data.toString()));
+    console.log(JSON.stringify(data.toString()).replace(/\\n/g, ''));
+});
