f9f25cf6fedf99d79a0a6a4835a35500b2401449
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-22 20:17:58 +0600

    fs is acting asynchronously

--- fs.js
+++ fs.js
@@ -5,7 +5,9 @@ console.log(fs);
 fs.readFile('demo.html', (err, data) => {
     if (err) throw err;
     console.log(data.toString());
-    console.log('File read');
     console.log(JSON.stringify(data.toString()));
     console.log(JSON.stringify(data.toString()).replace(/\\n/g, ''));
+    console.log('File read');
 });
+
+console.log('Hello Kishor');
