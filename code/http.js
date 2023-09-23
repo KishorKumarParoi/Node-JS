6c4dbe7d0b5ffef248825ab8d33d763b10d1a631
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-23 20:48:34 +0600

    response different according to req.url

--- code/http.js
+++ code/http.js
@@ -1,11 +1,21 @@
 import http from 'http';
 
-console.log(http);
-
-const server = http.createServer();
-server.on('connection', () => {
-    console.log('New connection...');
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    if (req.url === '/') {
+        res.write('Hello World!');
+        res.end();
+    } else if (req.url === '/about-me') {
+        res.write('My name is Kishor Paroi.');
+        res.write("I'm a software engineer.");
+        res.write('I like to code.');
+        res.end();
+    } else {
+        res.write('Not Found');
+        res.end();
+    }
 });
+
 server.listen(1000);
 
 console.log('Listening on port 1000...');
