7527e6fb0958f3f8443c625f65d1ec0406464ed2
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-24 18:18:47 +0600

    created http server, showed using readStream.pipe(res) and converted buffer msg to string

--- code/fs-new.js
+++ code/fs-new.js
@@ -3,7 +3,28 @@ import http from 'http';
 
 http.createServer((req, res) => {
     const readStream = fs.createReadStream('./demo.html', 'utf8');
-    const writeStream = fs.createWriteStream('./write.html');
-    readStream.pipe(res);
-    readStream.pipe(writeStream);
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write(`
+            <form action="/process" method="POST">
+                <input type="text" name="message" />
+                <button type="submit">Submit</button>
+            </form>
+        `);
+        res.end();
+    } else if (req.url === '/process' && req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            readStream.pipe(res);
+        });
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.write('Not Found!');
+        res.end();
+    }
 }).listen(3000);
