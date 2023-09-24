343c88fa990ab0205614ec583d4d9b4bf2a51fd2
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-24 17:51:43 +0600

    streaming of chunk of data

--- code/fs.js
+++ code/fs.js
@@ -1,10 +1,42 @@
 import fs from 'fs';
-
-// console.log(fs);
+import http from 'http';
 
 const ourReadStream = fs.createReadStream('./demo.html');
-console.log(ourReadStream);
 
+const arr = [];
 ourReadStream.on('data', (chunk) => {
-    console.log(chunk.toString());
+    arr.push(chunk.toString());
+    // console.log(chunk.toString());
 });
+
+// console.log(arr);
+
+http.createServer((req, res) => {
+    console.log(req.url);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    if (req.url === '/') {
+        res.write(`<form action="/process" method="POST">
+            <input type="text" name="message" />
+            <button type="submit">Submit</button>
+        </form>
+        `);
+        res.end();
+    } else if (req.url === '/process' && req.method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk);
+        });
+
+        req.on('end', () => {
+            console.log('Stream ended');
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            res.write('Thanks for submitting!');
+            res.end();
+        });
+    } else {
+        res.write('Not Found!');
+        res.end();
+    }
+}).listen(3000);
