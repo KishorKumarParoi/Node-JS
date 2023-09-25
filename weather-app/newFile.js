82d11b90a50a5f0ec02d4144726db57f1955ddfa
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2023-09-25 18:16:45 +0600

    learned readStream adn writeStream usage

--- weather-app/newFile.js
+++ weather-app/newFile.js
@@ -1,22 +1,11 @@
-/*
- * Title : Weather App
- * Description : A simple weather app that shows the current weather of a city
- * Author : Kishor Paroi
- * Date : 2023/09/25
- * Time : 10:40:13
- */
-
-// Dependencies
 import fs from 'fs';
 import http from 'http';
-// App object
-const app = {};
-const port = 3000;
+import { app, port } from '.';
 
 // Create server
 app.server = http
     .createServer((req, res) => {
-        const ourReadStream = fs.createReadStream('./lib/fetch/fetch.txt');
+        const ourReadStream = fs.createReadStream('./lib/fetch/fetch.txt', 'utf8');
         const writeStream = fs.createWriteStream('./lib/fetch/write.txt', 'utf8');
         ourReadStream.pipe(res);
         ourReadStream.pipe(writeStream);
@@ -32,6 +21,3 @@ app.server = http
         });
     })
     .listen(port);
-
-console.log(`Server is running on port ${port}`);
-console.log(app);
